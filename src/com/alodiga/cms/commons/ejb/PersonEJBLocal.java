b4400ba59ef11ff54c67cfb2719837031766cdca
Unknown <lgutierrez@alodiga.com>
2019-12-17 16:07:21 -0400

    Se eliminan metodos repetidos

--- src/com/alodiga/cms/commons/ejb/PersonEJBLocal.java
+++ src/com/alodiga/cms/commons/ejb/PersonEJBLocal.java
@@ -1,20 +1,17 @@
 package com.alodiga.cms.commons.ejb;
 
-import com.alodiga.cms.commons.exception.DisabledAccountException;
 import com.alodiga.cms.commons.exception.EmptyListException;
 import com.alodiga.cms.commons.exception.GeneralException;
 import com.alodiga.cms.commons.exception.NullParameterException;
 import com.alodiga.cms.commons.exception.RegisterNotFoundException;
 import com.cms.commons.genericEJB.DistributionGenericEJB;
 import com.cms.commons.genericEJB.EJBRequest;
-import com.cms.commons.models.Account;
 import com.cms.commons.models.FamilyReferences;
 import com.cms.commons.models.NaturalPerson;
 import com.cms.commons.models.Person;
 import com.cms.commons.models.PersonHasAddress;
 import com.cms.commons.models.PhonePerson;
 import com.cms.commons.models.PhoneType;
-import com.cms.commons.models.Program;
 import com.cms.commons.models.ApplicantNaturalPerson;
 import com.cms.commons.models.CardRequestNaturalPerson;
 import com.cms.commons.models.CivilStatus;
