f8f1e116a75b19ce3e46d99952382bdaad147015
Jesús Gómez <jedugomvil@hotmail.com>
2019-12-20 16:53:18 -0400

    Agregada consulta para obtener referencias familiares por solicitante

--- src/com/alodiga/cms/commons/ejb/UtilsEJBLocal.java
+++ src/com/alodiga/cms/commons/ejb/UtilsEJBLocal.java
@@ -145,11 +145,6 @@ public interface UtilsEJBLocal extends DistributionGenericEJB {
     public Product loadProduct(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
     public Product saveProduct(Product product) throws RegisterNotFoundException, NullParameterException, GeneralException;
 
-    //Request
-    public List<Request> getRequests(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
-    public Request loadRequest(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
-    public Request saveRequest(Request request) throws RegisterNotFoundException, NullParameterException, GeneralException;
-
     //Issuer
     public List<Issuer> getIssuers(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
     public Issuer loadIssuer(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
