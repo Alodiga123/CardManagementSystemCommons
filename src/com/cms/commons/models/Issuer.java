2494abe6d26cfc30b75025b2b1eed72774b543e5
Jesús Gómez <jedugomvil@hotmail.com>
2020-01-17 12:09:59 -0400

    Modificada la clase Issuer

--- src/com/cms/commons/models/Issuer.java
+++ src/com/cms/commons/models/Issuer.java
@@ -72,6 +72,10 @@ public class Issuer extends AbstractDistributionEntity implements Serializable{
     private String webSite;
     @Column(name = "faxNumber")
     private String faxNumber;
+    @Column(name = "personContactName")
+    private String personContactName;
+    @Column(name = "emailPersonContact")
+    private String emailPersonContact;
     @JoinColumn(name = "countryId", referencedColumnName = "id")
     @ManyToOne(optional = false)
     private Country countryId;
@@ -81,9 +85,6 @@ public class Issuer extends AbstractDistributionEntity implements Serializable{
     @JoinColumn(name = "issuerPersonId", referencedColumnName = "id")
     @OneToOne(optional = false)
     private Person issuerPersonId;
-    @JoinColumn(name = "contactPersonId", referencedColumnName = "id")
-    @OneToOne(optional = false)
-    private Person contactPersonId;
 
     public Issuer() {
     }
@@ -171,6 +172,22 @@ public class Issuer extends AbstractDistributionEntity implements Serializable{
     public void setFaxNumber(String faxNumber) {
         this.faxNumber = faxNumber;
     }
+    
+    public String getPersonContactName() {
+        return personContactName;
+    }
+
+    public void setPersonContactName(String personContactName) {
+        this.personContactName = personContactName;
+    }
+
+    public String getEmailPersonContact() {
+        return emailPersonContact;
+    }
+
+    public void setEmailPersonContact(String emailPersonContact) {
+        this.emailPersonContact = emailPersonContact;
+    }
 
     public Country getCountryId() {
         return countryId;
@@ -194,14 +211,6 @@ public class Issuer extends AbstractDistributionEntity implements Serializable{
 
     public void setIssuerPersonId(Person issuerPersonId) {
         this.issuerPersonId = issuerPersonId;
-    }
-
-    public Person getContactPersonId() {
-        return contactPersonId;
-    }
-
-    public void setContactPersonId(Person contactPersonId) {
-        this.contactPersonId = contactPersonId;
     }
 
     @Override
