4bcabeec8d73fe69ed5bd34dcdd55ed89f55b9fb
Unknown <yoanleon2718@gmail.com>
2020-01-23 10:29:56 -0430

    Ajuste en QueryConstants,  PersonEJB,  la Clase Issuer IssuerType

--- src/com/alodiga/cms/commons/ejb/PersonEJB.java
+++ src/com/alodiga/cms/commons/ejb/PersonEJB.java
@@ -18,6 +18,7 @@ import com.cms.commons.models.PhonePerson;
 import com.cms.commons.models.Profession;
 import com.cms.commons.models.FamilyReferences;
 import com.cms.commons.models.Issuer;
+import com.cms.commons.models.IssuerType;
 import com.cms.commons.models.KinShipApplicant;
 import com.cms.commons.models.LegalPersonHasLegalRepresentatives;
 import com.cms.commons.models.NaturalPerson;
@@ -106,5 +107,10 @@ public interface PersonEJB extends DistributionGenericEJB {
     public List<Issuer> getIssuer(EJBRequest request)throws EmptyListException, GeneralException, NullParameterException;
     public Issuer loadIssuer(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
     public Issuer saveIssuer (Issuer issuer)  throws RegisterNotFoundException, NullParameterException, GeneralException; 
-        
+    
+    //IssuerType
+    public List<IssuerType> getIssuerType(EJBRequest request)throws EmptyListException, GeneralException, NullParameterException;
+    public IssuerType loadIssuerType(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
+    public IssuerType saveIssuerType (IssuerType issuerType)  throws RegisterNotFoundException, NullParameterException, GeneralException; 
+
 }
