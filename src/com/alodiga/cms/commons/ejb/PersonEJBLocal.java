7452e23cfe7db68ea8a2c6bfa3043a71b0fe02fe
Unknown <lgutierrez@alodiga.com>
2020-03-04 15:30:44 -0400

    metodo getAdditionalInformationNaturalCustomeByCustomer nuevo

--- src/com/alodiga/cms/commons/ejb/PersonEJBLocal.java
+++ src/com/alodiga/cms/commons/ejb/PersonEJBLocal.java
@@ -157,6 +157,7 @@ public interface PersonEJBLocal extends DistributionGenericEJB {
     
     //AdditionalInformationNaturalCustomer
     public List<AdditionalInformationNaturalCustomer> getAdditionalInformationNaturalCustomer(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
+    public List<AdditionalInformationNaturalCustomer> getAdditionalInformationNaturalCustomeByCustomer(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
     public AdditionalInformationNaturalCustomer loadAdditionalInformationNaturalCustomer(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
     public AdditionalInformationNaturalCustomer saveAdditionalInformationNaturalCustomer(AdditionalInformationNaturalCustomer additionalInformationNaturalCustomer)  throws RegisterNotFoundException, NullParameterException, GeneralException;
             
