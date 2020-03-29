c67fe4d7fa77aa5dd0267920cf055544608e3fe0
Jesús Gómez <jedugomvil@hotmail.com>
2020-03-29 15:59:56 -0400

    Agregadas clases de seguridad de usuario y script de creacion

---
+++ src/com/cms/commons/models/PermissionData.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cms.commons.models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author jose
+ */
+@Entity
+@Table(name = "permission_data")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "PermissionData.findAll", query = "SELECT p FROM PermissionData p")
+    , @NamedQuery(name = "PermissionData.findById", query = "SELECT p FROM PermissionData p WHERE p.id = :id")
+    , @NamedQuery(name = "PermissionData.findByAlias", query = "SELECT p FROM PermissionData p WHERE p.alias = :alias")
+    , @NamedQuery(name = "PermissionData.findByDescription", query = "SELECT p FROM PermissionData p WHERE p.description = :description")})
+public class PermissionData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "alias")
+    private String alias;
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+    @JoinColumn(name = "languageId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Language languageId;
+    @JoinColumn(name = "permissionId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Permission permissionId;
+
+    public PermissionData() {
+    }
+
+    public PermissionData(Long id) {
+        this.id = id;
+    }
+
+    public PermissionData(Long id, String alias) {
+        this.id = id;
+        this.alias = alias;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Language getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Language languageId) {
+        this.languageId = languageId;
+    }
+
+    public Permission getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Permission permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PermissionData)) {
+            return false;
+        }
+        PermissionData other = (PermissionData) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.cms.commons.models.PermissionData[ id=" + id + " ]";
+    }
+    
+}
