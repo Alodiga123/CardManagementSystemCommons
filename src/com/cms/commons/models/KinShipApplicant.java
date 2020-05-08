cf622ff0b7d3f470d1b207b5564c46a00315abf1
yamealm <yamealm@gmail.com>
2020-05-07 23:39:20 -0300

    Cambios para Obtener Parentezo segun el pais

--- src/com/cms/commons/models/KinShipApplicant.java
+++ src/com/cms/commons/models/KinShipApplicant.java
@@ -16,6 +16,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -34,6 +36,7 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 @NamedQueries({
     @NamedQuery(name = "KinShipApplicant.findAll", query = "SELECT k FROM KinShipApplicant k")
     , @NamedQuery(name = "KinShipApplicant.findById", query = "SELECT k FROM KinShipApplicant k WHERE k.id = :id")
+    , @NamedQuery(name = "KinShipApplicant.findByCountryId", query = "SELECT k FROM KinShipApplicant k WHERE k.countryId.id = :countryId")    
     , @NamedQuery(name = "KinShipApplicant.findByDescription", query = "SELECT k FROM KinShipApplicant k WHERE k.description = :description")})
 public class KinShipApplicant extends AbstractDistributionEntity implements Serializable {
 
@@ -45,6 +48,9 @@ public class KinShipApplicant extends AbstractDistributionEntity implements Seri
     private Integer id;
     @Column(name = "description")
     private String description;
+    @JoinColumn(name = "countryId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Country countryId;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "kinShipApplicantId")
     private Collection<ApplicantNaturalPerson> applicantNaturalPersonCollection;
 
