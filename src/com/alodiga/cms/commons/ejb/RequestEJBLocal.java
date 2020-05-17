0f5e717f0d69811fff88115fd6008c38e91f85a4
YOAN <yoanleon2718@gmail.com>
2020-05-17 19:35:27 -0400

    Ajuste en los métodos de filtros

--- src/com/alodiga/cms/commons/ejb/RequestEJBLocal.java
+++ src/com/alodiga/cms/commons/ejb/RequestEJBLocal.java
@@ -66,6 +66,8 @@ public interface RequestEJBLocal extends DistributionGenericEJB {
     public List<CollectionsRequest> getCollectionsByRequest(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
     public CollectionsRequest loadCollectionsRequest(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
     public CollectionsRequest saveCollectionRequest(CollectionsRequest collectionRequest) throws NullParameterException, GeneralException;
+    public CollectionsRequest searchCollectionsRequest(String name) throws RegisterNotFoundException, NullParameterException, GeneralException;
+    public List<CollectionsRequest> getSearchCollectionsRequest(String name) throws EmptyListException, GeneralException, NullParameterException;
     
     //RequestHasCollectionsRequest
     public List<RequestHasCollectionsRequest> getRequestsHasCollectionsRequest(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
@@ -79,7 +81,8 @@ public interface RequestEJBLocal extends DistributionGenericEJB {
     public List<CollectionType> getCollectionTypeByCountry(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
     public CollectionType loadCollectionType(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
     public CollectionType saveCollectionType(CollectionType collectionType) throws NullParameterException, GeneralException;
-
+    public CollectionType searchCollectionType(String name) throws RegisterNotFoundException, NullParameterException, GeneralException;
+    
     //ReviewRequest
     public List<ReviewRequest> getReviewRequest(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
     public List<ReviewRequest> getReviewRequestByRequest(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
