31ab048309a758c12ffc93cd8fb7cc7f149f3050
Unknown <lgutierrez@alodiga.com>
2020-05-25 01:20:24 -0400

    constantes nuevas

--- src/com/cms/commons/util/Constants.java
+++ src/com/cms/commons/util/Constants.java
@@ -62,6 +62,8 @@ public class Constants {
     public static final int STATUS_CARDS_PERSONALIZED = 2;
     public static final int STATUS_CARDS_RENEWAL = 10;
     public static final int CARD_STATUS_DELIVERED = 7;
+    public static final int CARD_STATUS_INVENTORY = 4;
+    public static final int CARD_STATUS_ERROR = 5;
     public static final int ECONOMIC_ACTIVITY = 4;
     public static final int STATUS_REQUEST_COLLECTIONS_WITHOUT_APPROVAL = 7;
     public static final int STATUS_REQUEST_CUSTOMER_ASSIGNED_CARD = 8;
