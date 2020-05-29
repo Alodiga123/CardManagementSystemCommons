d5d83f62bb361b6264b91f5c4efac2af75e3fe2e
Unknown <lgutierrez@alodiga.com>
2020-05-29 17:19:47 -0400

    extends AbstractDistributionEntity

--- src/com/cms/commons/models/SystemFuncionality.java
+++ src/com/cms/commons/models/SystemFuncionality.java
@@ -5,6 +5,8 @@
  */
 package com.cms.commons.models;
 
+import com.alodiga.cms.commons.exception.TableNotFoundException;
+import com.cms.commons.genericEJB.AbstractDistributionEntity;
 import java.io.Serializable;
 import java.util.Collection;
 import javax.persistence.Basic;
@@ -33,10 +35,10 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 @Table(name = "systemFuncionality")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "SystemFuncionality.findAll", query = "SELECT s FROM SystemFuncionality s")
-    , @NamedQuery(name = "SystemFuncionality.findById", query = "SELECT s FROM SystemFuncionality s WHERE s.id = :id")
-    , @NamedQuery(name = "SystemFuncionality.findByName", query = "SELECT s FROM SystemFuncionality s WHERE s.name = :name")})
-public class SystemFuncionality implements Serializable {
+    @NamedQuery(name = "SystemFuncionality.findAll", query = "SELECT s FROM SystemFuncionality s"),
+    @NamedQuery(name = "SystemFuncionality.findById", query = "SELECT s FROM SystemFuncionality s WHERE s.id = :id"),
+    @NamedQuery(name = "SystemFuncionality.findByName", query = "SELECT s FROM SystemFuncionality s WHERE s.name = :name")})
+public class SystemFuncionality extends AbstractDistributionEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -129,5 +131,14 @@ public class SystemFuncionality implements Serializable {
     public String toString() {
         return "com.cms.commons.models.SystemFuncionality[ id=" + id + " ]";
     }
-    
+
+    @Override
+    public Object getPk() {
+        return getId();
+    }
+
+    @Override
+    public String getTableName() throws TableNotFoundException {
+        return super.getTableName(this.getClass());
+    }
 }
