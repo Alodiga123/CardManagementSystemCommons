4daf4944a2349a935a80233829a968f2622670b6
Jorge Pinto <jorgealepc@gmail.com>
2020-07-24 21:38:24 -0400

    Cambios de titulos de sub-menus CMS

---
+++ src/META-INF/sql/actualizar_opciones_menu_cms.sql
@@ -0,0 +1,25 @@
+-- Autor· Jorge Pinto
+-- Fecha 24 Julio 2020
+-- tabla permission_data
+
+-- Sub-Menu Opciones del sistema/System Options
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Features' WHERE `id`='281';
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Funcionalidades' WHERE `id`='282';
+
+
+--Sub-Menu Opciones del Sistema por Lenguaje/System Options by language
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Menu Options by Language' WHERE `id`='289';
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Opciones del Menu por Idioma' WHERE `id`='290';
+
+--Sub-Menu Opciones Principales por Lenguaje
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Opciones Principales por Idioma' WHERE `id`='306';
+
+--Sub-Menu  Perfiles de Usuario/
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Roles' WHERE `id`='313';
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Roles' WHERE `id`='314';
+
+
+--Sub-Menu roles de usuarios
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Assign Roles to Users' WHERE `id`='367';
+UPDATE `CardManagementSystem`.`permission_data` SET `alias`='Asignar Roles a Usuarios' WHERE `id`='368';
+
