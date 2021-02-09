9ac52042319da2633eceb5e7e17a9a724b442649
Jesús Gómez <jedugomvil@hotmail.com>
2021-02-09 12:43:49 -0400

    Agregadas las clases del esquema "Cierre Diario Transacciones"

--- src/META-INF/sql/Cambios_BD_CMS_Autorizador_Sprint2.sql
+++ src/META-INF/sql/Cambios_BD_CMS_Autorizador_Sprint2.sql
@@ -27,3 +27,110 @@ CREATE TABLE IF NOT EXISTS `CardManagementSystem`.`keyProperties` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+-- Agregar tablas para esquema de Proceso de Cierre Diario
+-- author: Jesús Gómez
+-- Fecha: 09/02/2021
+-- Crear tabla dailyClosing
+CREATE TABLE IF NOT EXISTS `CardManagementSystem`.`dailyClosing` (
+  `id` BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  `closingDate` DATE NOT NULL,
+  `closingStartTime` TIME NULL,
+  `closingEndTime` TIME NULL,
+  `originApplicationId` INT NOT NULL,
+  `totalTransactions` INT NULL,
+  `transactionsAmount` FLOAT NULL,
+  `createDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateDate` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_daily_closing_origin_application1_idx` (`originApplicationId` ASC),
+  CONSTRAINT `fk_daily_closing_origin_application1`
+    FOREIGN KEY (`originApplicationId`)
+    REFERENCES `CardManagementSystem`.`originApplication` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- Agregar FK dailyClosingId y el campo indClosed en las tablas transactionManagement y transactionManagementHistory
+ALTER TABLE `CardManagementSystem`.`transactionsManagement` 
+ADD COLUMN `indClosed` TINYINT(1) NULL AFTER `messageMiddlewareId`,
+ADD COLUMN `dailyClosingId` BIGINT NULL AFTER `indClosed`;
+SET FOREIGN_KEY_CHECKS = 0;
+ALTER TABLE `CardManagementSystem`.`transactionsManagement`
+ADD CONSTRAINT `fk_transactionsManagement_dailyClosing1`
+FOREIGN KEY (`dailyClosingId`)
+REFERENCES `CardManagementSystem`.`dailyClosing` (`id`)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE `CardManagementSystem`.`transactionsManagementHistory` 
+ADD COLUMN `indClosed` TINYINT(1) NULL AFTER `messageMiddlewareId`,
+ADD COLUMN `dailyClosingId` BIGINT NULL AFTER `indClosed`;
+ALTER TABLE `CardManagementSystem`.`transactionsManagementHistory`
+ADD CONSTRAINT `fk_transactionsManagementHistory_dailyClosing1`
+FOREIGN KEY (`dailyClosingId`)
+REFERENCES `CardManagementSystem`.`dailyClosing` (`id`)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+-- Crear tabla de calendar_days
+CREATE TABLE IF NOT EXISTS `CardManagementSystem`.`calendarDays` (
+  `id` BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  `countryId` INT NOT NULL,
+  `holidayDate` DATE NULL,
+  `description` VARCHAR(50) NULL,
+  `createDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateDate` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_calendarDays_country1_idx` (`countryId` ASC),
+  CONSTRAINT `fk_calendarDays_country1`
+    FOREIGN KEY (`countryId`)
+    REFERENCES `CardManagementSystem`.`country` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- Crear tablas para el Log de transacciones
+CREATE TABLE IF NOT EXISTS `CardManagementSystem`.`codeErrorTransactionLog` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(50) NULL,
+  `code` VARCHAR(50) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `CardManagementSystem`.`statusTransactionLog` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(50) NOT NULL,
+  `code` VARCHAR(10) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `CardManagementSystem`.`transactionLog` (
+  `id` BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  `transactionsManagementHistoryId` BIGINT(20) NOT NULL,
+  `statusTransactionLogId` INT NOT NULL,
+  `codeErrorTransactionLogId` INT NOT NULL,
+  `observations` VARCHAR(1500) NULL,
+  `createDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateDate` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_transactionLog_Transaction1_idx` (`transactionsManagementHistoryId` ASC),
+  INDEX `fk_transactionLog_StatusTransactionLog1_idx` (`statusTransactionLogId` ASC),
+  INDEX `fk_transactionLog_CodeErrorTransactionLog1_idx` (`codeErrorTransactionLogId` ASC),
+  CONSTRAINT `fk_transactionLog_Transaction1`
+    FOREIGN KEY (`transactionsManagementHistoryId`)
+    REFERENCES `CardManagementSystem`.`transactionsManagementHistory` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transactionLog_statusTransactionLog1`
+    FOREIGN KEY (`statusTransactionLogId`)
+    REFERENCES `CardManagementSystem`.`statusTransactionLog` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transactionLog_codeErrorTransactionLog1`
+    FOREIGN KEY (`codeErrorTransactionLogId`)
+    REFERENCES `CardManagementSystem`.`codeErrorTransactionLog` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ src/com/cms/commons/models/DailyClosing.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cms.commons.models;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ *
+ * @author jose
+ */
+@Entity
+@Table(name = "dailyClosing")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "DailyClosing.findAll", query = "SELECT d FROM DailyClosing d")
+    , @NamedQuery(name = "DailyClosing.findById", query = "SELECT d FROM DailyClosing d WHERE d.id = :id")
+    , @NamedQuery(name = "DailyClosing.findByClosingDate", query = "SELECT d FROM DailyClosing d WHERE d.closingDate = :closingDate")
+    , @NamedQuery(name = "DailyClosing.findByClosingStartTime", query = "SELECT d FROM DailyClosing d WHERE d.closingStartTime = :closingStartTime")
+    , @NamedQuery(name = "DailyClosing.findByClosingEndTime", query = "SELECT d FROM DailyClosing d WHERE d.closingEndTime = :closingEndTime")
+    , @NamedQuery(name = "DailyClosing.findByTotalTransactions", query = "SELECT d FROM DailyClosing d WHERE d.totalTransactions = :totalTransactions")
+    , @NamedQuery(name = "DailyClosing.findByTransactionsAmount", query = "SELECT d FROM DailyClosing d WHERE d.transactionsAmount = :transactionsAmount")
+    , @NamedQuery(name = "DailyClosing.findByCreateDate", query = "SELECT d FROM DailyClosing d WHERE d.createDate = :createDate")
+    , @NamedQuery(name = "DailyClosing.findByUpdateDate", query = "SELECT d FROM DailyClosing d WHERE d.updateDate = :updateDate")})
+public class DailyClosing implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "closingDate")
+    @Temporal(TemporalType.DATE)
+    private Date closingDate;
+    @Column(name = "closingStartTime")
+    @Temporal(TemporalType.TIME)
+    private Date closingStartTime;
+    @Column(name = "closingEndTime")
+    @Temporal(TemporalType.TIME)
+    private Date closingEndTime;
+    @Column(name = "totalTransactions")
+    private Integer totalTransactions;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "transactionsAmount")
+    private Float transactionsAmount;
+    @Column(name = "createDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+    @Column(name = "updateDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+    @JoinColumn(name = "originApplicationId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private OriginApplication originApplicationId;
+    @OneToMany(mappedBy = "dailyClosingId")
+    private Collection<TransactionsManagement> transactionsManagementCollection;
+    @OneToMany(mappedBy = "dailyClosingId")
+    private Collection<TransactionsManagementHistory> transactionsManagementHistoryCollection;
+
+    public DailyClosing() {
+    }
+
+    public DailyClosing(Long id) {
+        this.id = id;
+    }
+
+    public DailyClosing(Long id, Date closingDate) {
+        this.id = id;
+        this.closingDate = closingDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(Date closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public Date getClosingStartTime() {
+        return closingStartTime;
+    }
+
+    public void setClosingStartTime(Date closingStartTime) {
+        this.closingStartTime = closingStartTime;
+    }
+
+    public Date getClosingEndTime() {
+        return closingEndTime;
+    }
+
+    public void setClosingEndTime(Date closingEndTime) {
+        this.closingEndTime = closingEndTime;
+    }
+
+    public Integer getTotalTransactions() {
+        return totalTransactions;
+    }
+
+    public void setTotalTransactions(Integer totalTransactions) {
+        this.totalTransactions = totalTransactions;
+    }
+
+    public Float getTransactionsAmount() {
+        return transactionsAmount;
+    }
+
+    public void setTransactionsAmount(Float transactionsAmount) {
+        this.transactionsAmount = transactionsAmount;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public OriginApplication getOriginApplicationId() {
+        return originApplicationId;
+    }
+
+    public void setOriginApplicationId(OriginApplication originApplicationId) {
+        this.originApplicationId = originApplicationId;
+    }
+
+    @XmlTransient
+    @JsonIgnore
+    public Collection<TransactionsManagement> getTransactionsManagementCollection() {
+        return transactionsManagementCollection;
+    }
+
+    public void setTransactionsManagementCollection(Collection<TransactionsManagement> transactionsManagementCollection) {
+        this.transactionsManagementCollection = transactionsManagementCollection;
+    }
+
+    @XmlTransient
+    @JsonIgnore
+    public Collection<TransactionsManagementHistory> getTransactionsManagementHistoryCollection() {
+        return transactionsManagementHistoryCollection;
+    }
+
+    public void setTransactionsManagementHistoryCollection(Collection<TransactionsManagementHistory> transactionsManagementHistoryCollection) {
+        this.transactionsManagementHistoryCollection = transactionsManagementHistoryCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof DailyClosing)) {
+            return false;
+        }
+        DailyClosing other = (DailyClosing) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.cms.commons.models.DailyClosing[ id=" + id + " ]";
+    }
+    
+}
